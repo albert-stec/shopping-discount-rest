a9521892a95af03e92c6f29a447142efb4254a86
steca <stecalbert@gmail.com>
2024-11-29 22:29:05 +0100

    Implemented discount config, service and unit tests

--- src/main/kotlin/com/steca/shopping/controller/ProductController.kt
+++ src/main/kotlin/com/steca/shopping/controller/ProductController.kt
@@ -1,7 +1,8 @@
 package com.steca.shopping.controller
 
+import com.steca.shopping.config.DiscountConfig
 import com.steca.shopping.service.ProductService
-import java.util.UUID
+import java.util.*
 import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PathVariable
@@ -10,11 +11,13 @@ import org.springframework.web.bind.annotation.RestController
 
 @RestController
 @RequestMapping("/products")
-class ProductController(private val productService: ProductService) {
+class ProductController(
+    private val productService: ProductService,
+    private val discountConfig: DiscountConfig,
+) {
 
     @GetMapping("/{uuid}")
-    fun getProduct(@PathVariable uuid: UUID): ResponseEntity<String> {
-        val product = "test"
-        return ResponseEntity.ok(product)
+    fun getProduct(@PathVariable uuid: UUID): ResponseEntity<DiscountConfig?> {
+        return ResponseEntity.ok(discountConfig)
     }
 }
